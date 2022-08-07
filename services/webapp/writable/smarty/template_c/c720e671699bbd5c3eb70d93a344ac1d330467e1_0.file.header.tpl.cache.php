58b2a8a4529028a59d1e366ebbadd5baa6d169fc
Anupa Perera <anupa@wiredintelligence.com>
2022-08-07 18:31:27 +0530

    Stage 1 update of AMPT-CI

---
+++ services/webapp/writable/smarty/template_c/c720e671699bbd5c3eb70d93a344ac1d330467e1_0.file.header.tpl.cache.php
@@ -0,0 +1,46 @@
+<?php
+/* Smarty version 4.2.0, created on 2022-08-07 07:19:00
+  from '/var/www/app/Views/header.tpl' */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '4.2.0',
+  'unifunc' => 'content_62efadb4a72819_73991027',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    'c720e671699bbd5c3eb70d93a344ac1d330467e1' => 
+    array (
+      0 => '/var/www/app/Views/header.tpl',
+      1 => 1659871351,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_62efadb4a72819_73991027 (Smarty_Internal_Template $_smarty_tpl) {
+$_smarty_tpl->compiled->nocache_hash = '150274976162efadb4a6ae58_19111587';
+?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+    <!-- Bootstrap CSS -->
+    <link
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css"
+      rel="stylesheet"
+      integrity="sha384-wEmeIV1mKuiNpC+IOBjI7aAzPcEZeedi5yW5f2yOq55WWLwNGmvvx4Um1vskeMj0"
+      crossorigin="anonymous"
+    />
+
+    <title><?php echo $_smarty_tpl->tpl_vars['page_title']->value;?>
+</title>
+  </head>
+  <body>
+  <?php }
+}
